a1d57c6ece6760476f3c062272c147c05487065a
Tyler Heck <tyler.heck@gmail.com>
2016-01-03 13:48:03 -0500

    Use size information in renderer to update stroke sizing.

--- ballpoint/renderer/pipeline/stages/PointPopulationStage.swift
+++ ballpoint/renderer/pipeline/stages/PointPopulationStage.swift
@@ -12,14 +12,16 @@ import CoreGraphics
 
 
 struct PointPopulationStage: RenderPipelineStage {
-  /// The radius of each point.
-  private let REPLACE_ME: CGFloat = 1
-
-
   func process(inout scaffold: RenderScaffold, stroke: Stroke) {
     assert(
         stroke.points.count > 0,
         "Cannot populate scaffold points for an empty stroke.")
+
+    let pointRadii = stroke.points.map {
+      return
+          ((stroke.maximumWidth - stroke.minimumWidth) * $0.sizeFactor +
+           stroke.minimumWidth) / 2
+    }
 
     // If the stroke only has one point supply an arbitrary tangent line.
     if stroke.points.count == 1 {
@@ -27,7 +29,7 @@ struct PointPopulationStage: RenderPipelineStage {
           modelLocation: stroke.points[0].location,
           modelTangentLine:
               Line(slope: 0, throughPoint: stroke.points[0].location),
-          radius: REPLACE_ME))
+          radius: pointRadii[0]))
       return
     }
 
@@ -38,7 +40,7 @@ struct PointPopulationStage: RenderPipelineStage {
     scaffold.points.append(ScaffoldPoint(
         modelLocation: stroke.points[0].location,
         modelTangentLine: initialTangentLine,
-        radius: REPLACE_ME))
+        radius: pointRadii[0]))
 
     for i in 1..<(stroke.points.count - 1) {
       var tangentSlope: CGFloat
@@ -58,7 +60,7 @@ struct PointPopulationStage: RenderPipelineStage {
           modelLocation: stroke.points[i].location,
           modelTangentLine:
               Line(slope: tangentSlope, throughPoint: stroke.points[i].location),
-          radius: REPLACE_ME))
+          radius: pointRadii[i]))
     }
 
     // Two consecutive points in a stroke are assumed to never be equal.
@@ -68,7 +70,7 @@ struct PointPopulationStage: RenderPipelineStage {
     scaffold.points.append(ScaffoldPoint(
         modelLocation: stroke.points.last!.location,
         modelTangentLine: finalTangentLine,
-        radius: REPLACE_ME))
+        radius: pointRadii.last!))
 
     for i in 1..<scaffold.points.count {
       scaffold.points[i].ensurePointAlignment(scaffold.points[i - 1])
